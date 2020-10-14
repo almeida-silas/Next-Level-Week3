be50137369eae8a396a0db4e147d4f923a34b41e
Silas Almeida <silasadssn@gmail.com>
2020-10-14 15:03:24 -0300

    web

--- web/src/pages/OrphanagesMap/index.tsx
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -30,7 +30,7 @@ const OrphanagesMap: React.FC = () => {
         center={[-12.9606136, -38.4744683]}
         zoom={15}
         style={{ width: '100%', height: '100%' }}
-        >
+      >
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
         />
